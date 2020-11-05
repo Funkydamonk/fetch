cb617d75d96e2795b30097a945868ca28c1c93e7
Funkydamonk <45742035+Funkydamonk@users.noreply.github.com>
2020-11-05 20:56:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,13 +5,6 @@ $(document).ready(function() {
 	})
 });
 
-// Shoes
-// const title = document.getElementByTagName('title');
-// const header = document.querySelector('.header');
-// if (title.textContent === 'Shoes') {
-// 	header.classList.add('header--black');
-// }
-
 
 // pages 
 
@@ -27,127 +20,69 @@ for (const page of pages) {
 		page.classList.add('pages__item--bold');	
 	}
 }
-	// $(document).ready(function() {
-	// 	$('.navbar__list').slick({
-	// 		arrows: false,
-	// 		dots: false,
-	// 		// slidesToShow: 2,
-	// 		infinite: false,
-	// 		focusOnSelect: true,
-	// 		variableWidth: true,
-	// 		draggable: false,
-	// 		edgeFriction: 1,
-	// 		swipeToSlide: true,
-	// 		responsive:[
-	// 		{
-	// 			breakpoint: 768,
-	// 			settings: {
-	// 				arrows: false,
-	// 				slidesToShow: 3,
-	// 				edgeFriction: 0,05,
-	// 				draggable: true,
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 460,
-	// 			settings: {
-	// 				arrows: false,
-	// 				slidesToShow: 1,
-	// 				edgeFriction: 0,05,
-	// 				draggable: true,
-	// 			}
-	// 		}
-	// 		]
-	// 	})
-	// });
 
-// $(document).ready(function() {
-// 	$('.slider').slick({
-// 		arrows: true,
-// 		dots: false,
-// 		infinite: true,
-// 		autoplay: true,
-// 		autoplaySpeed: 5000,
-// 		pauseOnFocus: true,
-// 		pauseOnHover: true,
-// 		asNavFor: '.sliderbig',
-// 	})
-// });
+// anchor-slider + sub-slider
+$(document).ready(function() {
+	$('.anchor-slider').slick({
+		dots: false,
+		arrows: false,
+		asNavFor: '.sub-slider',
+		infinite: true,
+	})
+});
+$(document).ready(function() {
+	$('.sub-slider').slick({
+		dots: false,
+		arrows: false,
+		slidesToShow: 4,
+		asNavFor: '.anchor-slider',
+		focusOnSelect: true,
+		infinite: true,
+	})
+});
 
+// offer-slider
+$(document).ready(function() {
+	$('.offer__slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 4,
+	})
+});
 
-// //sliderpop
-// $(document).ready(function() {
-// 	$('.main__slider').slick({
-// 		arrows: true,
-// 		dots: false,
-// 		infinite: true,
-// 		slidesToShow: 3,
-// 	})
-// });
+//modal 
+const openModalButton = document.querySelector('.form__table-link');
+const closeModalButton = document.querySelector('.modal__close-button');
+const overlay = document.querySelector('.modal__overlay');
+const modal = document.querySelector('.modal');
 
+openModalButton.addEventListener('click', () => {
+	openModal()
+})
 
-// //validation 
-// const form = document.querySelector('.footer__form');
-// const email = document.querySelector('.footer__input-mail');
-// const check = document.querySelector('.footer__input-check');
-// const checkPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-// const success = document.querySelector('.footer__success');
+closeModalButton.addEventListener('click', () => {
+	closeModal()
+})
 
-// form.addEventListener('submit', (e) => {
-// 	e.preventDefault();
+function openModal() {
+	modal.classList.add('modal--active');
+	overlay.classList.add('modal__overlay--active');
+}
 
-// 	checkInputs();
-// });
+function closeModal() {
+	modal.classList.remove('modal--active');
+	overlay.classList.remove('modal__overlay--active');
+}
 
-// function checkInputs() {
-// 	const emailValue = email.value.trim();
+// closing modal with clicking on the void zone
+overlay.addEventListener('click', () => {
+	closeModal()
+})
 
-// 	if (emailValue === '') {
-// 			email.classList.add('footer__error');
-// 			check.classList.add('footer__input-check-visible');
-// 	} else if (!isEmail(emailValue)){
-// 		email.classList.add('footer__error');
-// 		check.classList.add('footer__input-check-visible');
-// 	} else {
-// 		form.style.visibility = 'hidden';
-// 		success.style.visibility = 'visible';
-// 	}
-// }
-
-// email.onfocus = function() {
-// 	if (email.classList.contains('footer__error') && check.classList.contains('footer__input-check-visible')) {
-// 		email.classList.remove('footer__error');
-// 		check.classList.remove('footer__input-check-visible');
-// 	}
-// }
-
-// function isEmail(emailValue) {
-// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailValue);
-// }
-
-// // Local storage
-// // email.oninput = () => {
-// // 	localStorage.setItem('emailLog', email.value);
-// // }
-
-// // if (email.value === '') {
-// // 	email.value = localStorage.getItem('emailLog');
-// // }
-// const message = document.querySelector('.footer__input-msg');
-
-// function setLog(logName, inputName) {
-// 	inputName.oninput = () => {
-// 		localStorage.setItem(logName, inputName.value);
-// 	}
-// }
-
-// function getLog(logName, inputName) {
-// 	if (inputName.value === '') {
-// 		inputName.value = localStorage.getItem(logName);
-// 	}
-// }
-
-// setLog('emailLog', email);
-// getLog('emailLog', email);
-// setLog('messageLog', message);
-// getLog('messageLog', message);
+// closing modal with an "esc" button
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    closeModal()
+  }
+});
